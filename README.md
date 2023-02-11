4ca0f5d09195fe4c4f3bf037e3b571a93898ee76
Tomasz Pawłowski <pawlowskitomasz91@gmail.com>
2023-02-11 20:09:56 +0100

    main function

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ An example of a website where scroll-progress.js is implemented to improve the u
 - ~~Add needed files, simple skeleton and style of document.~~
 - ~~Add sample articles with long text feed to create optimum environment for script~~
 - ~~Main functionality of script without visual exposition~~
-- Add scroll-progress element and styling it
-- Add progress update function and display it on element
+- ~~Add scroll-progress element and styling it~~
+- ~~Add progress update function and display it on element~~
 - Describe project
 - Add animation animation
 - Review code
--- scroll-progress.js
+++ scroll-progress.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', (DOMContentLoadedEvent) => {
     console.log('DOM fully loaded and parsed in', Math.round(DOMContentLoadedEvent.timeStamp), "ms, with:", DOMContentLoadedEvent.srcElement.all.length, "elements.");
-    onscroll = function(event){
-        let currentScrollPosition = document.documentElement.scrollTop,
-            documentHeigh = document.documentElement.scrollHeight,
-            webBrowserWindowHeigh = document.documentElement.clientHeight;
-        // console.log(Math.round((currentScrollPosition / (documentHeigh - webBrowserWindowHeigh)) * 100));
-    };
+    onscroll = () => {updateScroll()};
+    updateScroll();
 });
 
+function updateScroll(){
+    let currentScrollPosition = document.documentElement.scrollTop,
+        documentHeigh = document.documentElement.scrollHeight,
+        webBrowserWindowHeigh = document.documentElement.clientHeight,
+        scrollProgressText = document.querySelector('.scrollProgressBox');
+    scrollProgressText.textContent = (Math.round((currentScrollPosition / (documentHeigh - webBrowserWindowHeigh)) * 100) + "%");
+    // console.log(Math.round((currentScrollPosition / (documentHeigh - webBrowserWindowHeigh)) * 100));
+};
